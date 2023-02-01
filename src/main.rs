coderedart/rust-sdl2-wasm

use etk crates as backend and run egui

src/main.rs
@@ -1,197 +1,68 @@
-use std::cell::RefCell;
-use std::mem;
-use std::os::raw::{c_int, c_uchar, c_void};
-use std::ptr::null_mut;
+use egui_backend::egui::Window;
+use egui_backend::{BackendConfig, EguiUserApp, GfxBackend, WindowBackend};
+use egui_render_glow::GlowBackend;
+use egui_window_sdl2::Sdl2Backend;
+use mlua::Lua;
 
-use sdl2::gfx::primitives::DrawRenderer;
-
-// pub mod gl {
-//     include!(concat!(env!("OUT_DIR"), "/gl_bindings.rs"));
-// }
-// use self::gl::types::*;
-
-#[allow(non_camel_case_types)]
-type em_callback_func = unsafe extern "C" fn();
-
-extern "C" {
-    // This extern is built in by Emscripten.
-    pub fn emscripten_run_script_int(x: *const c_uchar) -> c_int;
-    pub fn emscripten_cancel_main_loop();
-    pub fn emscripten_set_main_loop(
-        func: em_callback_func,
-        fps: c_int,
-        simulate_infinite_loop: c_int,
-    );
+/// This is our userdata.
+struct UserAppData {
+    /// lua code that we can edit live and execute.
+    code: String,
+    /// well.. lua vm to execute the above code.
+    lua_vm: mlua::Lua,
+    /// glow (rusty opengl wrapper) renderer to draw egui.. and other stuff with opengl. use three-d backend for high level features like gltf/meshes/materials/lighting etc..
+    glow_backend: GlowBackend,
+    /// egui context
+    egui_context: egui_backend::egui::Context,
 }
+/// just some default lua code to show in text editor
+pub const LUA_CODE: &str = r#"
+print('hello from lua');
+"#;
 
-thread_local!(static MAIN_LOOP_CALLBACK: RefCell<Option<Box<dyn FnMut()>>> = RefCell::new(None));
-
-pub fn set_main_loop_callback<F: 'static>(callback: F)
-where
-    F: FnMut(),
-{
-    MAIN_LOOP_CALLBACK.with(|log| {
-        *log.borrow_mut() = Some(Box::new(callback));
-    });
-
-    unsafe {
-        emscripten_set_main_loop(wrapper::<F>, 0, 1);
+// we care generic over the window backend. so, we can just decide at runtime which backend to use. eg: winit, glfw3, sdl2 are provided by `etk`
+impl<WB: egui_backend::WindowBackend> EguiUserApp<WB> for UserAppData {
+    // these are used by some default trait method implementations to abstract out the common parts like providing egui input or drawing egui output etc..
+    type UserGfxBackend = GlowBackend;
+    fn get_gfx_backend(&mut self) -> &mut Self::UserGfxBackend {
+        &mut self.glow_backend
     }
-
-    extern "C" fn wrapper<F>()
-    where
-        F: FnMut(),
-    {
-        MAIN_LOOP_CALLBACK.with(|z| {
-            if let Some(ref mut callback) = *z.borrow_mut() {
-                callback();
+    fn get_egui_context(&mut self) -> egui_backend::egui::Context {
+        self.egui_context.clone()
+    }
+    // the only function we care about. add whatever gui code you want.
+    fn gui_run(&mut self, egui_context: &egui_backend::egui::Context, _window_backend: &mut WB) {
+        Window::new("hello window").show(&egui_context, |ui| {
+            ui.code_editor(&mut self.code);
+            if ui.button("run code").clicked() {
+                if let Err(e) = self.lua_vm.load(&self.code).exec() {
+                    eprintln!("failed to run lua code: {e}")
+                }
             }
         });
+        // this tells sdl2 to immediately repaint after vsync. otherwise, it will sleep, waiting for events -> causing unresponsive browser tab.
+        egui_context.request_repaint();
     }
 }
-pub const LUA_CODE: &str = r#"
-lua_fn = function(click)
-    print("current color: "..click)
-end
-"#;
-const DANCING_TTF: &[u8] = include_bytes!("dancing.ttf");
-struct Owner {
-    ttx: sdl2::ttf::Sdl2TtfContext,
-}
 fn main() {
-    println!("Startup");
-
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
-
-    video_subsystem
-        .gl_attr()
-        .set_context_profile(sdl2::video::GLProfile::GLES);
-    video_subsystem.gl_attr().set_context_major_version(2);
-    video_subsystem.gl_attr().set_context_minor_version(0);
-
-    let window = video_subsystem
-        .window("rust-sdl2-wasm", 1920, 1080)
-        .position_centered()
-        .opengl()
-        .build()
-        .unwrap();
-
-    let gl_context = window.gl_create_context().unwrap();
-    window.gl_make_current(&gl_context).unwrap();
-    let mut canvas = window
-        .into_canvas()
-        .build()
-        .expect("failed to create canvas from window");
-    let ttx = Box::new(sdl2::ttf::init().expect("failed to init ttf"));
-    let ttx = Box::leak(ttx);
-    let dancing = ttx
-        .load_font_from_rwops(sdl2::rwops::RWops::from_bytes(DANCING_TTF).unwrap(), 16)
-        .expect("failed to load font hack");
-
-    let lua_vm = mlua::Lua::new();
-    let mut lua_code = "print('hello')".to_string();
-    lua_vm
-        .load(LUA_CODE)
-        .exec()
-        .expect("failed to execute lua code");
-    let mut font_surface = dancing
-        .render(&lua_code)
-        .blended(sdl2::pixels::Color::RGB(200, 15, 40))
-        .expect("failed to render hack font");
-
-    println!("setting loop callback");
-    let mut timer = sdl_context.timer().unwrap();
-    let mut event_pump = sdl_context.event_pump().unwrap();
-    event_pump.enable_event(sdl2::event::EventType::MouseButtonDown);
-    set_main_loop_callback(move || {
-        // use glow::HasContext;
-        let milliseconds = timer.ticks();
-        // lets chagne color every 3 seconds
-        // let elapsed = milliseconds % 3000;
-        // let elapsed = elapsed as f32;
-        // clamp to 0.0-1.0
-        let color = milliseconds as f32 / 1000.0;
-        let r = (color.sin() * 255.0) as u8;
-        let g = (color.cos() * 255.0) as u8;
-        let b = (color.tan() * 255.0) as u8;
-        // unsafe {
-        //     gtx.clear_color(1.0, 1.0, 1.0, 1.0);
-        //     gtx.clear(glow::COLOR_BUFFER_BIT);
-        // }
-        canvas.set_draw_color(sdl2::pixels::Color::RGB(255, 255, 255));
-        canvas.clear();
-        let color = sdl2::pixels::Color::RGB(r, g, b);
-        canvas.set_draw_color(color);
-        canvas
-            .fill_rect(sdl2::rect::Rect::new(100, 100, 300, 150))
-            .expect("failed to draw rect");
-        let texture_creator = canvas.texture_creator();
-        let font_texture = font_surface
-            .as_texture(&texture_creator)
-            .expect("failed to ccreate texture of font surface");
-        canvas
-            .copy(
-                &font_texture,
-                None,
-                Some(sdl2::rect::Rect::new(
-                    40,
-                    40,
-                    font_surface.width(),
-                    font_surface.height(),
-                )),
-            )
-            .expect("failed to copy font texture to canvas");
-        // canvas.string(300, 300, "hello from sdl2", color).unwrap();
-        while let Some(ev) = event_pump.poll_event() {
-            match ev {
-                sdl2::event::Event::MouseButtonDown { .. } => match lua_vm.load(&lua_code).exec() {
-                    Ok(_) => {
-                        println!("lua_code executed successfully");
-                    }
-                    Err(e) => {
-                        println!("lua_code failed to run. error: {e:?}");
-                    }
-                },
-                sdl2::event::Event::KeyDown {
-                    keycode,
-                    keymod,
-                    repeat,
-                    ..
-                } => {
-                    if keymod == sdl2::keyboard::Mod::NOMOD {
-                        if let Some(keycode) = keycode {
-                            if match keycode {
-                                sdl2::keyboard::Keycode::Backspace => {
-                                    lua_code.pop();
-                                    true
-                                }
-                                sdl2::keyboard::Keycode::Return => {
-                                    lua_code.push('\n');
-                                    true
-                                }
-                                _ => false,
-                            } {
-                                if lua_code.len() > 0 {
-                                    font_surface = dancing
-                                        .render(&lua_code)
-                                        .blended(sdl2::pixels::Color::RGB(200, 15, 40))
-                                        .expect("failed to render hack font");
-                                }
-                            }
-                        }
-                    }
-                }
-                sdl2::event::Event::TextInput { text, .. } => {
-                    lua_code.push_str(&text);
-                    font_surface = dancing
-                        .render(&lua_code)
-                        .blended(sdl2::pixels::Color::RGB(200, 15, 40))
-                        .expect("failed to render hack font");
-                }
-                _ => {}
-            }
-        }
-        canvas.present();
-    });
+    // init logging
+    tracing_subscriber::fmt().init();
+    // just create a new backend. ask sdl for an opengl window because we chose glow backend. on vulkan/dx/metal(desktop), we would choose non-gl window.
+    let mut sdl2_backend = Sdl2Backend::new(
+        Default::default(),
+        BackendConfig {
+            gfx_api_type: egui_backend::GfxApiType::GL,
+        },
+    );
+    // create a opengl backend.
+    let glow_backend = GlowBackend::new(&mut sdl2_backend, Default::default());
+    // create our app data
+    let app = UserAppData {
+        code: LUA_CODE.to_string(),
+        lua_vm: Lua::new(),
+        glow_backend,
+        egui_context: Default::default(),
+    };
+    // enter event loop and run forever :)
+    sdl2_backend.run_event_loop(app);
 }

coderedart/egui_glow_glfw_emscripten

src/main.rs
@@ -1,197 +1,68 @@
-use std::cell::RefCell;
-use std::mem;
-use std::os::raw::{c_int, c_uchar, c_void};
-use std::ptr::null_mut;
+use egui_backend::egui::Window;
+use egui_backend::{BackendConfig, EguiUserApp, GfxBackend, WindowBackend};
+use egui_render_glow::GlowBackend;
+use egui_window_glfw_passthrough::GlfwBackend;
+use mlua::Lua;
 
-use sdl2::gfx::primitives::DrawRenderer;
-
-// pub mod gl {
-//     include!(concat!(env!("OUT_DIR"), "/gl_bindings.rs"));
-// }
-// use self::gl::types::*;
-
-#[allow(non_camel_case_types)]
-type em_callback_func = unsafe extern "C" fn();
-
-extern "C" {
-    // This extern is built in by Emscripten.
-    pub fn emscripten_run_script_int(x: *const c_uchar) -> c_int;
-    pub fn emscripten_cancel_main_loop();
-    pub fn emscripten_set_main_loop(
-        func: em_callback_func,
-        fps: c_int,
-        simulate_infinite_loop: c_int,
-    );
+/// This is our userdata.
+struct UserAppData {
+    /// lua code that we can edit live and execute.
+    code: String,
+    /// well.. lua vm to execute the above code.
+    lua_vm: mlua::Lua,
+    /// glow (rusty opengl wrapper) renderer to draw egui.. and other stuff with opengl. use three-d backend for high level features like gltf/meshes/materials/lighting etc..
+    glow_backend: GlowBackend,
+    /// egui context
+    egui_context: egui_backend::egui::Context,
 }
+/// just some default lua code to show in text editor
+pub const LUA_CODE: &str = r#"
+print('hello from lua');
+"#;
 
-thread_local!(static MAIN_LOOP_CALLBACK: RefCell<Option<Box<dyn FnMut()>>> = RefCell::new(None));
-
-pub fn set_main_loop_callback<F: 'static>(callback: F)
-where
-    F: FnMut(),
-{
-    MAIN_LOOP_CALLBACK.with(|log| {
-        *log.borrow_mut() = Some(Box::new(callback));
-    });
-
-    unsafe {
-        emscripten_set_main_loop(wrapper::<F>, 0, 1);
+// we care generic over the window backend. so, we can just decide at runtime which backend to use. eg: winit, glfw3, sdl2 are provided by `etk`
+impl<WB: egui_backend::WindowBackend> EguiUserApp<WB> for UserAppData {
+    // these are used by some default trait method implementations to abstract out the common parts like providing egui input or drawing egui output etc..
+    type UserGfxBackend = GlowBackend;
+    fn get_gfx_backend(&mut self) -> &mut Self::UserGfxBackend {
+        &mut self.glow_backend
     }
-
-    extern "C" fn wrapper<F>()
-    where
-        F: FnMut(),
-    {
-        MAIN_LOOP_CALLBACK.with(|z| {
-            if let Some(ref mut callback) = *z.borrow_mut() {
-                callback();
+    fn get_egui_context(&mut self) -> egui_backend::egui::Context {
+        self.egui_context.clone()
+    }
+    // the only function we care about. add whatever gui code you want.
+    fn gui_run(&mut self, egui_context: &egui_backend::egui::Context, _window_backend: &mut WB) {
+        Window::new("hello window").show(&egui_context, |ui| {
+            ui.code_editor(&mut self.code);
+            if ui.button("run code").clicked() {
+                if let Err(e) = self.lua_vm.load(&self.code).exec() {
+                    eprintln!("failed to run lua code: {e}")
+                }
             }
         });
+        // this tells window to immediately repaint after vsync. otherwise, it will sleep, waiting for events -> causing unresponsive browser tab.
+        egui_context.request_repaint();
     }
 }
-pub const LUA_CODE: &str = r#"
-lua_fn = function(click)
-    print("current color: "..click)
-end
-"#;
-const DANCING_TTF: &[u8] = include_bytes!("dancing.ttf");
-struct Owner {
-    ttx: sdl2::ttf::Sdl2TtfContext,
-}
 fn main() {
-    println!("Startup");
-
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
-
-    video_subsystem
-        .gl_attr()
-        .set_context_profile(sdl2::video::GLProfile::GLES);
-    video_subsystem.gl_attr().set_context_major_version(2);
-    video_subsystem.gl_attr().set_context_minor_version(0);
-
-    let window = video_subsystem
-        .window("rust-sdl2-wasm", 1920, 1080)
-        .position_centered()
-        .opengl()
-        .build()
-        .unwrap();
-
-    let gl_context = window.gl_create_context().unwrap();
-    window.gl_make_current(&gl_context).unwrap();
-    let mut canvas = window
-        .into_canvas()
-        .build()
-        .expect("failed to create canvas from window");
-    let ttx = Box::new(sdl2::ttf::init().expect("failed to init ttf"));
-    let ttx = Box::leak(ttx);
-    let dancing = ttx
-        .load_font_from_rwops(sdl2::rwops::RWops::from_bytes(DANCING_TTF).unwrap(), 16)
-        .expect("failed to load font hack");
-
-    let lua_vm = mlua::Lua::new();
-    let mut lua_code = "print('hello')".to_string();
-    lua_vm
-        .load(LUA_CODE)
-        .exec()
-        .expect("failed to execute lua code");
-    let mut font_surface = dancing
-        .render(&lua_code)
-        .blended(sdl2::pixels::Color::RGB(200, 15, 40))
-        .expect("failed to render hack font");
-
-    println!("setting loop callback");
-    let mut timer = sdl_context.timer().unwrap();
-    let mut event_pump = sdl_context.event_pump().unwrap();
-    event_pump.enable_event(sdl2::event::EventType::MouseButtonDown);
-    set_main_loop_callback(move || {
-        // use glow::HasContext;
-        let milliseconds = timer.ticks();
-        // lets chagne color every 3 seconds
-        // let elapsed = milliseconds % 3000;
-        // let elapsed = elapsed as f32;
-        // clamp to 0.0-1.0
-        let color = milliseconds as f32 / 1000.0;
-        let r = (color.sin() * 255.0) as u8;
-        let g = (color.cos() * 255.0) as u8;
-        let b = (color.tan() * 255.0) as u8;
-        // unsafe {
-        //     gtx.clear_color(1.0, 1.0, 1.0, 1.0);
-        //     gtx.clear(glow::COLOR_BUFFER_BIT);
-        // }
-        canvas.set_draw_color(sdl2::pixels::Color::RGB(255, 255, 255));
-        canvas.clear();
-        let color = sdl2::pixels::Color::RGB(r, g, b);
-        canvas.set_draw_color(color);
-        canvas
-            .fill_rect(sdl2::rect::Rect::new(100, 100, 300, 150))
-            .expect("failed to draw rect");
-        let texture_creator = canvas.texture_creator();
-        let font_texture = font_surface
-            .as_texture(&texture_creator)
-            .expect("failed to ccreate texture of font surface");
-        canvas
-            .copy(
-                &font_texture,
-                None,
-                Some(sdl2::rect::Rect::new(
-                    40,
-                    40,
-                    font_surface.width(),
-                    font_surface.height(),
-                )),
-            )
-            .expect("failed to copy font texture to canvas");
-        // canvas.string(300, 300, "hello from sdl2", color).unwrap();
-        while let Some(ev) = event_pump.poll_event() {
-            match ev {
-                sdl2::event::Event::MouseButtonDown { .. } => match lua_vm.load(&lua_code).exec() {
-                    Ok(_) => {
-                        println!("lua_code executed successfully");
-                    }
-                    Err(e) => {
-                        println!("lua_code failed to run. error: {e:?}");
-                    }
-                },
-                sdl2::event::Event::KeyDown {
-                    keycode,
-                    keymod,
-                    repeat,
-                    ..
-                } => {
-                    if keymod == sdl2::keyboard::Mod::NOMOD {
-                        if let Some(keycode) = keycode {
-                            if match keycode {
-                                sdl2::keyboard::Keycode::Backspace => {
-                                    lua_code.pop();
-                                    true
-                                }
-                                sdl2::keyboard::Keycode::Return => {
-                                    lua_code.push('\n');
-                                    true
-                                }
-                                _ => false,
-                            } {
-                                if lua_code.len() > 0 {
-                                    font_surface = dancing
-                                        .render(&lua_code)
-                                        .blended(sdl2::pixels::Color::RGB(200, 15, 40))
-                                        .expect("failed to render hack font");
-                                }
-                            }
-                        }
-                    }
-                }
-                sdl2::event::Event::TextInput { text, .. } => {
-                    lua_code.push_str(&text);
-                    font_surface = dancing
-                        .render(&lua_code)
-                        .blended(sdl2::pixels::Color::RGB(200, 15, 40))
-                        .expect("failed to render hack font");
-                }
-                _ => {}
-            }
-        }
-        canvas.present();
-    });
+    // init logging
+    tracing_subscriber::fmt().init();
+    // just create a new backend. ask window backend for an opengl window because we chose glow backend. on vulkan/dx/metal(desktop), we would choose non-gl window.
+    let mut window_backend = GlfwBackend::new(
+        Default::default(),
+        BackendConfig {
+            gfx_api_type: egui_backend::GfxApiType::GL,
+        },
+    );
+    // create a opengl backend.
+    let glow_backend = GlowBackend::new(&mut window_backend, Default::default());
+    // create our app data
+    let app = UserAppData {
+        code: LUA_CODE.to_string(),
+        lua_vm: Lua::new(),
+        glow_backend,
+        egui_context: Default::default(),
+    };
+    // enter event loop and run forever :)
+    window_backend.run_event_loop(app);
 }